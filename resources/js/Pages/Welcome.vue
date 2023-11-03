9397de2522e98a18105eada34377894ac2db9cbf
JerryColao <137563759+JerryColao@users.noreply.github.com>
2023-11-03 15:00:48 -0600

    Footer, mision y vision

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -79,17 +79,11 @@ onMounted(fetchData)
                 </div>
 
                 <div class="text-justify">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pulvinar vehicula neque, sed
-                        lacinia nisl rhoncus sit amet. Pellentesque at facilisis metus. Quisque ut purus tortor. Sed
-                        orci ex, tempor in dui ut, laoreet molestie ex. Donec placerat, augue quis porttitor iaculis,
-                        arcu neque imperdiet diam, accumsan rhoncus neque augue at felis.
+                    <p>Promote good nutrition for the people of El Salvador through recipes and exercise routines to motivate them to change their habits through the Easy Meals platform.
                     </p>
                 </div>
                 <div class="text-justify">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pulvinar vehicula neque, sed
-                        lacinia nisl rhoncus sit amet. Pellentesque at facilisis metus. Quisque ut purus tortor. Sed
-                        orci ex, tempor in dui ut, laoreet molestie ex. Donec placerat, augue quis porttitor iaculis,
-                        arcu neque imperdiet diam, accumsan rhoncus neque augue at felis.
+                    <p>To be a primary source for those seeking inspiration and guidance on the path to a healthier and more physically active life. We want to be known as a trusted platform that offers healthy and delicious recipes, effective exercise routines and above all give the best experience to our community.
                     </p>
                 </div>
 
@@ -117,6 +111,34 @@ onMounted(fetchData)
                 </a>
             </h1>
         </div>
+        <footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+        <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+            <div class="sm:flex sm:items-center sm:justify-between">
+                <a href="https://flowbite.com" class="flex items-center mb-4 sm:mb-0">
+                    <img src="../../img/Logos/Easy-Meals_actualizado.png" class="h-20 mr-3" alt="Flowbite Logo" />
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
+                </a>
+                <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                    <li>
+                        <a href="#" class="mr-4 hover:underline md:mr-6 ">E-mail</a>
+                    </li>
+                    <li>
+                        <a href="#" class="mr-4 hover:underline md:mr-6">Whatsapp</a>
+                    </li>
+                    <li>
+                        <a href="#" class="mr-4 hover:underline md:mr-6 ">Facebook</a>
+                    </li>
+                    <li>
+                        <a href="#" class="hover:underline">Instagram</a>
+                    </li>
+                </ul>
+            </div>
+            <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">2023<a href="https://flowbite.com/" class="hover:underline"></a></span>
+        </div>
+    </footer>
+
+
     </Layout>
 </template>
 
